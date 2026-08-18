e5c0f4d075f608c9472e1675fd9dad298c3baa45
savri <savahp123@gmail.com>
2026-08-18 13:15:04 +0700

    ==============

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
-export const API_BASE_URL = axios.create({
-  baseURL: 'http://localhost:8000/api'
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api',
 })
+
+export default api
