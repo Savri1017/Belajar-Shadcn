4a9a7b92f97494a7b6446e8a3785a735d46caf73
Sava Rizky Anugerah <savahp123@gmail.com>
2026-08-19 11:03:12 +0700

    feat: add shadcn checkbox component

---
+++ src/components/ui/checkbox/Checkbox.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { CheckboxIndicator, CheckboxRoot } from "reka-ui"
+import { Check } from "lucide-vue-next"
+import { cn } from "@/lib/utils"
+
+const props = defineProps({
+  modelValue: { type: [Boolean, String], required: false },
+  disabled: { type: Boolean, required: false },
+  class: {
+    type: [Boolean, null, String, Object, Array],
+    required: false,
+    skipCheck: true,
+  },
+})
+
+const emit = defineEmits(["update:modelValue"])
+</script>
+
+<template>
+  <CheckboxRoot
+    :model-value="props.modelValue"
+    :disabled="props.disabled"
+    :class="cn(
+      'peer size-4 shrink-0 rounded-[4px] border border-primary shadow-xs outline-none transition-shadow focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+      props.class,
+    )"
+    @update:model-value="emit('update:modelValue', $event)"
+  >
+    <CheckboxIndicator class="flex items-center justify-center text-current">
+      <Check class="size-3.5" />
+    </CheckboxIndicator>
+  </CheckboxRoot>
+</template>
